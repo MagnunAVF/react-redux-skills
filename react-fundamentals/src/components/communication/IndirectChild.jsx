cbe796e498271b496c752e016db21fd7e7eff76d
Magnun A V F <magnunavf@gmail.com>
2021-07-25 00:03:18 -0300

    Use useState hook in indirect communication components

--- react-fundamentals/src/components/communication/IndirectChild.jsx
+++ react-fundamentals/src/components/communication/IndirectChild.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 export default (props) => {
   const callbackFunc = props.whenClicked;
+
+  const generateAge = () => parseInt(Math.random() * 20) + 50;
+
+  const generateNerd = () => Math.random() > 0.5;
+
   return (
     <div>
       <div>Child</div>
-      <button onClick={(_) => callbackFunc("João", 53, true)}>
+      <button
+        onClick={(_) => callbackFunc("João", generateAge(), generateNerd())}
+      >
         Send Infos!
       </button>
     </div>
--- react-fundamentals/src/components/communication/IndirectParent.jsx
+++ react-fundamentals/src/components/communication/IndirectParent.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import IndirectChild from "./IndirectChild";
 
 export default (props) => {
+  const [name, setName] = useState("?");
+  const [age, setAge] = useState(0);
+  const [nerd, setNerd] = useState(false);
+
   function receiveInfos(name, age, nerd) {
-    console.log(`Name: ${name}, Age: ${age}, IsNerd: ${nerd}`);
+    setName(name);
+    setAge(age);
+    setNerd(nerd);
   }
+
   return (
     <div>
       <div>Parent</div>
+      <div><span>Name - Age - Is Nerd?</span></div>
+      <div>
+        <span>{name} - </span>
+        <span><strong>{age}</strong> - </span>
+        <span>{nerd ? "True" : "False"}</span>
+      </div>
       <IndirectChild whenClicked={receiveInfos}></IndirectChild>
     </div>
   );
